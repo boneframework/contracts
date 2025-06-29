539877a5dc223ed962628ff4ea8078c253c5ec7d
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-06-29 16:14:52 +0200

    add ApiCollectionInterface

---
+++ src/Collection/ApiCollectionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bone\Contracts\Collection;
+
+use Psr\Http\Message\UriInterface;
+
+interface ApiCollectionInterface
+{
+    public function getUri(): UriInterface;
+    public function getPage(): int;
+    public function getTotalPages(): int;
+}
