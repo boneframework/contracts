68b8e9a5fc7dccb39332c12f78f62391c55fe0e3
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-08-18 15:54:26 +0200

    update signature

--- src/View/ViewEngineInterface.php
+++ src/View/ViewEngineInterface.php
@@ -7,6 +7,6 @@ namespace Bone\View;
 interface ViewEngineInterface
 {
     public function __construct($viewPath, $extension = null);
-    public function render($view, array $vars = []): string;
+    public function render($view, array $vars = []);
     public function addFolder($name, $directory, $fallback = false);
 }
