96f4e7d8ec9b697b57b93cfd065d6a9d9d670e18
Derek Stephen McLean <delboy1978uk@gmail.com>
2026-01-29 00:12:29 +0100

    translator interface

---
+++ src/Contracts/Service/TranslatorInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bone\Contracts\Service;
+
+interface TranslatorInterface
+{
+    public function translate(string $message, ?string $textDomain = null, ?string $locale = null): string;
+}
