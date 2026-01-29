db6bb49e80e3feb7464f1f6caf6092fb009c3fb8
Derek Stephen McLean <delboy1978uk@gmail.com>
2026-01-29 15:12:26 +0100

    add getLocale

--- src/Contracts/Service/TranslatorInterface.php
+++ src/Contracts/Service/TranslatorInterface.php
@@ -7,4 +7,5 @@ namespace Bone\Contracts\Service;
 interface TranslatorInterface
 {
     public function translate(string $message, ?string $textDomain = null, ?string $locale = null): string;
+    public function getLocale(): string;
 }
